c1645e087ee00aaad66f9a76d915736440894606
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-12-27 08:19:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const { appConfig } = require('./config');
+const { appConfig, Logger } = require('./config');
 // const { appConfig } = config;
 
+global.logger = Logger.createLogger({ label: 'CHAINFUNDIT API' });
+
 const express = require('express');
+
+logger.info('text info');
+logger.warn('text warn');
+logger.error('text error');
+logger.debug('text debug');
 
 const app = express();
 
